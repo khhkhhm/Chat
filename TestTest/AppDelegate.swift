487009e1def046cf67e1c4705fac321ba6d02880
khhkhhm <61198109+khhkhhm@users.noreply.github.com>
2020-02-18 19:20:47 +0300

    Update AppDelegate.swift

--- TestTest/AppDelegate.swift
+++ TestTest/AppDelegate.swift
@@ -35,13 +35,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        PrintLog(#function)
         return true
         
     }
     
     func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        PrintLog(#function, nil)
         return true
     }
     
